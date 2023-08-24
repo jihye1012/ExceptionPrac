d24df2ca005b4e8cd1f54eefe0187759f73d799c
Jihye Yang <s2127@e-mirim.hs.kr>
2023-08-24 14:41:28 +0900

    커스텀 예외 객처 생성 및 활용한 예외처리

--- ExceptionPrac/Program.cs
+++ ExceptionPrac/Program.cs
@@ -8,6 +8,11 @@ namespace ExceptionPrac
 {
     internal class Program
     {
+        //입력숫자는 0 이상 999이하여야함.
+        public class WrongNumberException : Exception {
+        }
+            
+        
         static void Main(string[] args)
         {
             string[] array = { "가", "나" };
@@ -16,8 +21,12 @@ namespace ExceptionPrac
             try
             {
                 int input = int.Parse(Console.ReadLine());
+                if (input < 0 || input > 999)
+                    throw new WrongNumberException();
                 Console.WriteLine("입력한 위치의 값은 '" + array[input] + "'입니다.");
-            }catch(IndexOutOfRangeException ex)
+
+            }
+                catch(IndexOutOfRangeException ex)
             {
                 Console.WriteLine("인덱스 범위를 벗어났습니다.");
             }
@@ -25,12 +34,19 @@ namespace ExceptionPrac
             {
                 Console.WriteLine("숫자 입력:");
             }
+            catch(WrongNumberException ex)
+            {
+                Console.WriteLine("올바른 숫자 입력 : (0~999)");
+            }
 
             catch (Exception ex) 
             {
                 Console.WriteLine(ex.GetType());
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
+            }finally
+            {
+                Console.WriteLine("옳게 실행되든 예외가 발생하든 실행됨!");
             }
            
             
